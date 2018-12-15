9688fb58de60db4f68eef786a14366ad2b3802cc
JP Lynch <JPLynch35@gmail.com>
2018-12-15 11:57:37 -0500

    Add /articles route and action

---
+++ spec/features/user_sees_all_articles_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'a user' do
+  describe 'visits articles index page' do
+    it 'sees all articles' do
+      article_1 = Article.create(title: 'Title_1', body: 'Body_1')
+      article_2 = Article.create(title: 'Title_2', body: 'Body_2')
+
+      visit articles_path
+
+      expect(page).to have_content('article_1.title')
+      expect(page).to have_content('article_2.title')
+    end
+  end
+end

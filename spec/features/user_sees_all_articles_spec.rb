6d001f0fc957c06f1548f985b7a271448421ac92
JP Lynch <JPLynch35@gmail.com>
2018-12-15 12:29:25 -0500

    Add link to create new article on article index page

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 describe 'a user' do
   describe 'visits articles index page' do
+    before :each do
+      @article_1 = Article.create(title: 'Title_1', body: 'Body_1')
+      @article_2 = Article.create(title: 'Title_2', body: 'Body_2')
+    end
     it 'sees all articles' do
-      article_1 = Article.create(title: 'Title_1', body: 'Body_1')
-      article_2 = Article.create(title: 'Title_2', body: 'Body_2')
-
       visit articles_path
 
-      expect(page).to have_link(article_1.title)
-      expect(page).to have_link(article_2.title)
+      expect(page).to have_link(@article_1.title)
+      expect(page).to have_link(@article_2.title)
+    end
+    it 'sees a link to create a new article' do
+      visit articles_path
+
+      expect(page).to have_link('Create a New Article')
     end
   end
 end
